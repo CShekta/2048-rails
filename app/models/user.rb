e7835e91f36ca264cd0dacde3d0a0040867eff8d
Claire <ClaireS29@gmail.com>
2016-02-08 15:53:37 -0800

    added omniauth all the env deets

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ActiveRecord::Base
+
+  def self.find_or_create_from_omniauth(auth_hash)
+    user = self.find_by(uid: auth_hash["uid"], provider: auth_hash["provider"])
+    if !user.nil?
+      return user
+    else
+      user = User.new
+      user.uid        = auth_hash["uid"]
+      user.provider   = auth_hash["provider"]
+      user.name       = auth_hash["info"]["name"]
+      if user.save
+        user
+      else
+        nil
+      end
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160208233712) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "uid"
+    t.string   "username"
+    t.string   "provider"
+    t.string   "email"
+    t.string   "avatar_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
