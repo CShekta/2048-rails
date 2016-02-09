a3b91809121f1eac734cc2227bca119d328d65ef
Claire <ClaireS29@gmail.com>
2016-02-08 16:52:44 -0800

    added application header with bootstrap jquery, ccreated routes to home and game index pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,8 @@ class User < ActiveRecord::Base
       user = User.new
       user.uid        = auth_hash["uid"]
       user.provider   = auth_hash["provider"]
-      user.name       = auth_hash["info"]["name"]
+      user.username   = auth_hash["info"]["name"]
+      user.email      = auth_hash["info"]["email"]
       if user.save
         user
       else
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "site#index"
   match "/auth/:provider/callback", to: "sessions#create", via: [:get, :post]
+  get "/game", to: "site#game"
 
 end
